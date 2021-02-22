0881f19dcc54035d1aa3ede470f495f1fed9f349
Alessandro Chitolina <alekitto@gmail.com>
2021-02-22 13:24:33 +0100

    test phpdocumentor path/notPath

--- tests/Iterator/PhpDocumentorIteratorTest.php
+++ tests/Iterator/PhpDocumentorIteratorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kcs\ClassFinder\Tests\Iterator;
 
+use Kcs\ClassFinder\Fixtures\Psr0;
 use Kcs\ClassFinder\Fixtures\Psr4;
 use Kcs\ClassFinder\Iterator\PhpDocumentorIterator;
 use phpDocumentor\Reflection\Php\Class_;
@@ -16,7 +17,7 @@ use function realpath;
 
 class PhpDocumentorIteratorTest extends TestCase
 {
-    public function testIteratorShouldWork()
+    public function testIteratorShouldWork(): void
     {
         $iterator = new PhpDocumentorIterator(
             realpath(__DIR__ . '/../../data/Composer/Psr4')
@@ -36,5 +37,50 @@ class PhpDocumentorIteratorTest extends TestCase
         self::assertInstanceOf(Interface_::class, $classes[Psr4\FooInterface::class]);
         self::assertArrayHasKey(Psr4\FooTrait::class, $classes);
         self::assertInstanceOf(Trait_::class, $classes[Psr4\FooTrait::class]);
+    }
+
+    public function testComposerIteratorShouldFilterNotIntersectingPath(): void
+    {
+        $iterator = new PhpDocumentorIterator(
+            realpath(__DIR__ . '/../../data/Composer')
+        );
+
+        $iterator->in([__DIR__ . '/../..' . '/data/Composer/Psr?']);
+        $classes = iterator_to_array($iterator);
+
+        self::assertArrayHasKey(Psr4\BarBar::class, $classes);
+        self::assertInstanceOf(Class_::class, $classes[Psr4\BarBar::class]);
+        self::assertArrayHasKey(Psr4\Foobar::class, $classes);
+        self::assertInstanceOf(Class_::class, $classes[Psr4\Foobar::class]);
+        self::assertArrayHasKey(Psr4\AbstractClass::class, $classes);
+        self::assertInstanceOf(Class_::class, $classes[Psr4\AbstractClass::class]);
+        self::assertArrayHasKey(Psr4\SubNs\FooBaz::class, $classes);
+        self::assertInstanceOf(Class_::class, $classes[Psr4\SubNs\FooBaz::class]);
+        self::assertArrayHasKey(Psr4\FooInterface::class, $classes);
+        self::assertInstanceOf(Interface_::class, $classes[Psr4\FooInterface::class]);
+        self::assertArrayHasKey(Psr4\FooTrait::class, $classes);
+        self::assertInstanceOf(Trait_::class, $classes[Psr4\FooTrait::class]);
+
+        self::assertArrayHasKey(Psr0\BarBar::class, $classes);
+        self::assertInstanceOf(Class_::class, $classes[Psr0\BarBar::class]);
+        self::assertArrayHasKey(Psr0\Foobar::class, $classes);
+        self::assertInstanceOf(Class_::class, $classes[Psr0\Foobar::class]);
+        self::assertArrayHasKey(Psr0\SubNs\FooBaz::class, $classes);
+        self::assertInstanceOf(Class_::class, $classes[Psr0\SubNs\FooBaz::class]);
+
+        $iterator->path(['/Sub.*/']);
+        $classes = iterator_to_array($iterator);
+
+        self::assertEquals([
+            Psr0\SubNs\FooBaz::class,
+            Psr4\SubNs\FooBaz::class,
+        ], array_keys($classes));
+
+        $iterator->path(['/Sub.*/'])->notPath(['/sr4/']);
+        $classes = iterator_to_array($iterator);
+
+        self::assertEquals([
+            Psr0\SubNs\FooBaz::class,
+        ], array_keys($classes));
     }
 }
