869d5a5e44868fbbc7339656ebaf83f628a299bb
Alessandro Chitolina <alekitto@gmail.com>
2024-06-13 11:04:06 +0200

    fix(tests): ignore composer.json files in nested vendor folders

--- tests/install-deps.php
+++ tests/install-deps.php
@@ -3,37 +3,43 @@
 declare(strict_types=1);
 
 foreach (glob(__DIR__ . '/*') as $path) {
-    if (is_dir($path)) {
-        $files = new RecursiveIteratorIterator(
-            new RecursiveCallbackFilterIterator(
-                new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS),
-                static fn (SplFileInfo $file): bool => $file->getBasename()[0] !== '.',
-            ),
-            RecursiveIteratorIterator::LEAVES_ONLY | RecursiveIteratorIterator::CHILD_FIRST,
-        );
+    if (! is_dir($path)) {
+        continue;
+    }
 
-        foreach ($files as $filepath => $info) {
-            if (! $info->isFile()) {
-                continue;
-            }
+    $files = new RecursiveIteratorIterator(
+        new RecursiveCallbackFilterIterator(
+            new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS),
+            static fn (SplFileInfo $file): bool => $file->getBasename()[0] !== '.',
+        ),
+        RecursiveIteratorIterator::LEAVES_ONLY | RecursiveIteratorIterator::CHILD_FIRST,
+    );
 
-            if ($info->getFilename() === 'composer.json') {
-                if (basename(dirname($info->getPath())) === 'vendor') {
-                    continue;
-                }
+    foreach ($files as $filepath => $info) {
+        if (! $info->isFile()) {
+            continue;
+        }
 
-                $descriptorspec = [STDIN, STDOUT, STDERR];
-                $proc = proc_open(trim('composer install ' . getenv('COMPOSER_FLAGS') ?: ''), $descriptorspec, $pipes, $info->getPath());
-                for ($running = true; $running;) {
-                    $status = proc_get_status($proc);
-                    $running = $status['running'];
-                    $exitcode = $status['exitcode'];
-                }
+        if ($info->getFilename() !== 'composer.json') {
+            continue;
+        }
 
-                if ($exitcode !== 0) {
-                    return;
-                }
-            }
+        if (str_contains(dirname($info->getPath()), '/vendor/')) {
+            continue;
+        }
+
+        echo 'Processing ' . $info->getPath() . "...\n";
+
+        $descriptorspec = [STDIN, STDOUT, STDERR];
+        $proc = proc_open(trim('composer install ' . getenv('COMPOSER_FLAGS') ?: ''), $descriptorspec, $pipes, $info->getPath());
+        for ($running = true; $running;) {
+            $status = proc_get_status($proc);
+            $running = $status['running'];
+            $exitcode = $status['exitcode'];
+        }
+
+        if ($exitcode !== 0) {
+            return;
         }
     }
 }
